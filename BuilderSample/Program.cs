28e80070ce434374de59396f5c5c810e7e10041e
liupenghao <haochen214@163.com>
2019-06-28 23:17:29 +0800

    配置文件和测试类

--- BuilderSample/Program.cs
+++ BuilderSample/Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Configuration;
+using System.Reflection;
+
 
 namespace BuilderSample
 {
@@ -14,6 +17,22 @@ namespace BuilderSample
         /// <param name="args"></param>
         static void Main(string[] args)
         {
+            ActorBuilder ab;    //针对抽象建造者编程
+            // 读取配置文件
+            string builderType = ConfigurationManager.AppSettings["builder"];
+            //反射生成对象
+            ab = (ActorBuilder)Assembly.Load("BuilderSample").CreateInstance(builderType);
+            ActorController ac = new ActorController();
+            Actor actor;
+            actor = ac.Construct(ab);   // 通过指挥者创建完整的建造者对象
+
+            Console.WriteLine("{0}的外观", actor.Type);
+            Console.WriteLine("性别：{0}", actor.Sex);
+            Console.WriteLine("面容：{0}", actor.Face);
+            Console.WriteLine("服装：{0}", actor.Costume);
+            Console.WriteLine("发型：{0}", actor.HairStyle);
+
+            Console.ReadLine();
         }
     }
 }
