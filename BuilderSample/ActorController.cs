d6b2fa0504f6d233da71dafe0e9cfc5110056638
liupenghao <haochen214@163.com>
2019-06-28 23:10:22 +0800

    创建指挥者

---
+++ BuilderSample/ActorController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuilderSample
+{
+    /// <summary>
+    /// 角色控制器，充当具体指挥者
+    /// </summary>
+    class ActorController
+    {
+        public Actor Construct(ActorBuilder ab)
+        {
+            Actor actor;
+            ab.BuildType();
+            ab.BuildSex();
+            ab.BuildFace();
+            ab.BuildCostume();
+            ab.BuildHairstyle();
+            actor = ab.CreateActor();
+            return actor;
+        }
+    }
+}
