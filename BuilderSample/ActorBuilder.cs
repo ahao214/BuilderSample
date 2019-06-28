9123fbce772c87d99ea519a2c7bb3b090ebd1482
liupenghao <haochen214@163.com>
2019-06-28 22:54:54 +0800

    添加游戏角色抽象建造者

---
+++ BuilderSample/ActorBuilder.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuilderSample
+{
+    /// <summary>
+    /// 角色创建者：抽象建造者
+    /// </summary>
+    abstract class ActorBuilder
+    {
+        protected Actor actor = new Actor();
+        public abstract void BuildType();
+        public abstract void BuildSex();
+        public abstract string BuildFace();
+        public abstract string BuildCostume();
+        public abstract string BuildHairstyle();
+
+        //工厂方法,返回一个完整的游戏角色对象
+        public Actor CreateActor()
+        {
+            return actor;
+        }
+    }
+}
